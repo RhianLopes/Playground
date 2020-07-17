dde08c0722bff53943507b553fa63879a8a800d3
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2020-07-17 20:14:13 -0300

    feat: learning about types, variables, converters and do a speed challenge

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -1,5 +1,54 @@
 import UIKit
 
-var str = "Hello, playground"
+// ============================================================================================
 
-print("alo")
+//String
+var string: String = ""
+
+string = "test"
+string = "wow"
+
+//Number Int
+var integer: Int = 10
+integer = integer + 5
+integer = integer * 2
+integer = 0
+
+//Number Double (15 numbers after dot 0.123456789123456)
+var double: Double
+double = 2.456
+double = double / 2
+double = 0
+
+//Number float (6 numbers after dot 0.123456)
+var float: Float
+float = 2.34321
+float = float * 23
+float = 0
+
+//Print
+print("The Integer value is " + String( integer ))
+print("The Double value is " + String( double ))
+print("The Float value is " + String( float ))
+print("The String value is " + string)
+
+//Converters ...
+String( integer )
+Int( double )
+Double( integer )
+Float( double )
+
+// ============================================================================================
+
+var firstNumber: Int
+var secondNumber: Int
+var result: Int
+
+firstNumber = 2
+secondNumber = 3
+
+result = firstNumber + secondNumber
+
+print("The result is: " + String( result ))
+
+// ============================================================================================
