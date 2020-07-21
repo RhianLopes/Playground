e98fe846957325541f1b4db47ff7e4c621cab09f
Rhian Lopes da Costa <rhianlopes63@gmail.com>
2020-07-21 20:47:20 -0300

    feat: introduce some new operators in swift

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-// ###################################################################################################
+// ============================================================================================
 
 //String
 var string: String = ""
@@ -76,12 +76,139 @@ var integerArray: [Int] = [] // []
 integerArray.append(0) // [ 0 ]
 integerArray.append(1) // [ 0, 1 ]
 
-// ###################################################################################################
+// ============================================================================================
 
 var list = Set<String>()
 
-list.insert("pizza")
-list.insert("juice")
-list.insert("juice")
+list.insert("pizza") // [ "pizza" ]
+list.insert("juice") // [ "pizza", "juice" ]
+list.insert("juice") // [ "pizza", "juice" ] the Set<>() dont repeat the value
 
 print(list)
+
+// ============================================================================================
+
+var animals = [String: String]() // key: String, value: String
+
+animals["bear"] = "Big animal and its eat fish" // key: bear, value: Big animal and its eat fish
+animals["dog"] = "The best human friend" // key: dog, value: The best human friend
+
+print( animals["dog"]! )
+
+
+var mounths = [Int: String]() // key: Int, value: String
+
+mounths[1] = "Janeiro"
+mounths[2] = "Fevereiro"
+mounths[3] = "Março"
+// ...
+
+print( mounths[2]! ) //Fevereiro
+
+
+// ============================================================================================
+
+/*
+ - Addition (+)
+ - Subtraction (-)
+ - Multiplication (*)
+ - Division (/)
+ */
+
+var a = 5
+var b = 5
+var total: Int
+
+total = a + b // 10
+total = a - b // 0
+total = a * b // 25
+total = a / b // 1
+
+// ============================================================================================
+
+
+/*
+- Equal to (a == b)
+- Not equal to (a != b)
+- Greater than (a > b)
+- Less than (a < b)
+- Greater than or equal to (a >= b)
+- Less than or equal to (a <= b)
+ */
+
+var z = 1
+var u = 2
+
+z == u //false
+z != u //true
+z > u //false
+z < u //true
+z >= u //false
+z <= u //true
+
+/*
+ - && ( and )
+ - || ( or )
+ */
+
+var price = 100
+
+price > 50 && price < 150 // true
+price < 50 && price < 150 // false
+price < 50 || price > 150 // false
+
+price > 50 || price < 150 // true
+price < 50 || price < 150 // true
+price < 50 || price > 150 // false
+
+var age = 20
+
+age >= 18 && age <= 26 // true
+age = 27
+age >= 18 && age <= 23 // false
+age = 17
+age >= 18 && age <= 23 // false
+
+// ============================================================================================
+
+/*
+ - if else
+*/
+
+var priceCar = 350
+var typeCar = ""
+
+if (priceCar >= 100 && priceCar <= 200) {
+    typeCar = "popular car"
+} else if (priceCar > 200 && priceCar <= 300) {
+    typeCar = "medium car"
+} else if (priceCar > 300 && priceCar <= 400) {
+    typeCar = "super car"
+} else {
+    typeCar = "none"
+}
+
+print( typeCar ) // super car
+
+var agePerson = 18
+var ageResult: String
+
+if (agePerson >= 18 ) {
+    ageResult = "AUTHORIZED ACCESS"
+} else {
+    ageResult = "DENIED ACCESS"
+}
+
+print( ageResult ) // AUTHORIZED ACCESS
+
+// ============================================================================================
+
+/*
+- Loop - for
+*/
+
+// for var i in 0..<5 {
+//     print("Number " + String( i ))
+// }
+
+var commentary: [String]
